d32ce8dfa4bd861a0de6d33e03abf2973d5aa845
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-09-30 18:20:03 +0000

    Toggle chart functionality added

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,6 +3,20 @@ import { checkAuth, getNewToken } from "./auth.js";
 //Check if there is an access token
 checkAuth();
 
+// Reduce numbers into a more readable form
+const shortenNum = (num) => {
+  if (num >= 100000000) {
+    return `${Math.floor(num / 100000000)}B`;
+  } else if (num >= 1000000) {
+    return `${Math.floor(num / 1000000)}M`;
+  } else if (num >= 1000) {
+    return `${Math.floor(num / 1000)}K`;
+  } else {
+    return num;
+  }
+};
+
+// Load Dashboard Information
 const getDashboardInfo = async () => {
   const result = await fetch("https://freddy.codesubmit.io/dashboard", {
     method: "GET",
@@ -12,20 +26,11 @@ const getDashboardInfo = async () => {
     },
   });
 
-  const shortenNum = (num) => {
-    if (num >= 1000000) {
-      return `${Math.floor(num / 1000000)}M`;
-    } else if (num >= 1000) {
-      return `${Math.floor(num / 1000)}K`;
-    } else {
-      return num;
-    }
-  };
-
   const data = await result.json();
 
   console.log(data);
 
+  // If Token has expired, refresh it
   if (data.msg === "Token has expired") {
     const sessionRefreshed = await getNewToken();
     if (sessionRefreshed) {
@@ -33,6 +38,8 @@ const getDashboardInfo = async () => {
     } else {
       window.location.href = "./login.html";
     }
+
+    // If Token is not expired, display dashboard with relevant info
   } else if (data.dashboard) {
     // Update Dashboard Information
     const todayTotalSpan = document.getElementById("today-total");
@@ -64,12 +71,19 @@ const getDashboardInfo = async () => {
       await data.dashboard.sales_over_time_year[2].orders
     );
 
-    const data_values = [];
+    const data_values_week = [];
     for (const item in data.dashboard.sales_over_time_week) {
-      data_values.push(data.dashboard.sales_over_time_week[item].total);
+      data_values_week.push(data.dashboard.sales_over_time_week[item].total);
     }
 
-    console.log(data_values);
+    console.log(data_values_week);
+
+    const data_values_year = [];
+    for (const item in data.dashboard.sales_over_time_year) {
+      data_values_year.push(data.dashboard.sales_over_time_year[item].total);
+    }
+
+    console.log(data_values_year);
 
     // Create Chart
     const labels = [
@@ -89,7 +103,7 @@ const getDashboardInfo = async () => {
           label: "Revenue (last 7 days)",
           backgroundColor: "rgb(255, 99, 132)",
           borderColor: "rgb(255, 99, 132)",
-          data: data_values,
+          data: data_values_week,
         },
       ],
     };
@@ -101,6 +115,49 @@ const getDashboardInfo = async () => {
     };
 
     const myChart = new Chart(document.getElementById("myChart"), config);
+
+    //Switch chart function
+    const switchChart = (event) => {
+      let showSecondChart = event.target.checked;
+      const chartTitle = document.getElementById("chart-title");
+      myChart.data.labels = showSecondChart
+        ? [
+            "This Month",
+            "Last Month",
+            "Month 3",
+            "Month 4",
+            "Month 5",
+            "Month 6",
+            "Month 7",
+            "Month 8",
+            "Month 9",
+            "Month 10",
+            "Month 11",
+            "Month 12",
+          ]
+        : ["Today", "Yesterday", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"];
+      myChart.data.datasets = [
+        {
+          label: showSecondChart
+            ? "Revenue (last 12 months)"
+            : "Revenue (last 7 days)",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: showSecondChart ? data_values_year : data_values_week,
+        },
+      ];
+      myChart.update();
+      chartTitle.textContent = showSecondChart
+        ? "Revenue (last 12 months)"
+        : "Revenue (last 7 days)";
+    };
+
+    const toggleBtn = document.getElementById("toggleBtn");
+
+    // Update chat on toggle of toggleBtn
+    toggleBtn.addEventListener("change", switchChart);
+
+    // If token is invalid, redirect to login page
   } else {
     window.location.href = "./login.html";
   }
