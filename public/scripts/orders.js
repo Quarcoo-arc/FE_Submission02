71fedd0a5b24c70ad81bc54a0ab0a627e9692048
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-10-01 11:14:10 +0000

    Search orders functionality added

--- public/scripts/orders.js
+++ public/scripts/orders.js
@@ -3,9 +3,8 @@ import { checkAuth, getNewToken } from "./auth.js";
 //Check if there is an access token
 checkAuth();
 
-// Load Orders Info
-const getOrdersInfo = async () => {
-  const result = await fetch("https://freddy.codesubmit.io/orders", {
+const fetchOrders = async (url) => {
+  const result = await fetch(url, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -14,6 +13,39 @@ const getOrdersInfo = async () => {
   });
 
   const data = await result.json();
+
+  return data;
+};
+
+const populateTable = (data) => {
+  const tableBody = document.getElementsByTagName("tbody")[0];
+
+  let tableRows = ``;
+
+  data.forEach((order) => {
+    tableRows += `
+            <tr>
+              <td>${order.product.name}</td>
+              <td>${new Date(order.created_at).toUTCString()}</td>
+              <td>${order.currency}${order.total}</td>
+              <td class="${
+                order.status === "processing"
+                  ? "text-red"
+                  : order.status === "delivered"
+                  ? "text-green"
+                  : ""
+              }">${
+      order.status.charAt(0).toUpperCase() + order.status.slice(1)
+    }</td>
+            </tr>`;
+  });
+
+  tableBody.innerHTML = tableRows;
+};
+
+// Load Orders Info
+const getOrdersInfo = async () => {
+  const data = await fetchOrders("https://freddy.codesubmit.io/orders");
 
   console.log(data);
 
@@ -28,22 +60,45 @@ const getOrdersInfo = async () => {
 
     // If Token is not expired, populate orders page with relevant info
   } else if (data.orders) {
-    const tableBody = document.getElementsByTagName("tbody")[0];
+    populateTable(data.orders);
 
-    let tableRows = ``;
+    // Search for specific orders
 
-    data.orders.forEach((order) => {
-      tableRows += `
-            <tr>
-              <td>${order.product.name}</td>
-              <td>${new Date(order.created_at).toUTCString()}</td>
-              <td>${order.currency}${order.total}</td>
-              <td>${order.status}</td>
-            </tr>`;
-    });
+    const searchOrders = async (event) => {
+      event.preventDefault();
 
-    tableBody.innerHTML = tableRows;
+      const keyword = searchForm.searchTerm.value;
 
+      searchForm.searchTerm.value = "";
+
+      console.log(keyword);
+
+      const data = await fetchOrders(
+        `https://freddy.codesubmit.io/orders?q=${keyword}`
+      );
+
+      if (data.msg === "Token has expired") {
+        const sessionRefreshed = await getNewToken();
+        if (sessionRefreshed) {
+          data = await fetchOrders(
+            `https://freddy.codesubmit.io/orders?q=${keyword}`
+          );
+        } else {
+          window.location.href = "./login.html";
+        }
+
+        // If Token is not expired, populate orders page with relevant info
+      } else if (data.orders) {
+        console.log(data);
+        populateTable(data.orders);
+      } else {
+        window.location.href = "./login.html";
+      }
+    };
+
+    const searchForm = document.getElementById("searchForm");
+
+    searchForm.addEventListener("submit", searchOrders);
     // If token is invalid, redirect to login page
   } else {
     window.location.href = "./login.html";
